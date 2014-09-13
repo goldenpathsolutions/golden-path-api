d9de0ee5fe23cd986f9126a63f7cdb5aa38f6b10
Patrick Jackson <pjackson@goldenpathsolutions.com>
2014-09-13 13:43:03 -0700

    Fix typo and add phpdoc tags to index header

--- golden-path-api.php
+++ golden-path-api.php
@@ -14,14 +14,19 @@
  * This plugin contains a number of useful tools and functions for developers.
  * These include the following.
  * 
- * - Enqueue Manager - Makes t easier to only include scripts and styles on the
+ * - Enqueue Manager - Makes it easier to only include scripts and styles on the
  *      pages you want by wrapping the enqueues with some page-aware logic.
  *      @see Enqueue_Manager
  *      @see Enqueue_Script_Item
  *      @see Enqueue_Style_Item
  * 
+ * @category golden-path-api
+ * @author: Patrick Jackson <pjackson@goldenpathsolutions.com>
+ * @version: 1.0.0
+ * @copyright 2014 Golden Path Solutions, Inc.
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, v2.0
  * 
- * 
+ * @todo need to finish documenting everything
  * 
  */
 
